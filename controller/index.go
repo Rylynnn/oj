85eca8d8f539c48020c486b65f7e96b7cf8085db
Qiyu Li <0x9357@gmail.com>
2015-12-01 13:46:41 +0800

    add comment for rm warning

--- controller/index.go
+++ controller/index.go
@@ -6,12 +6,15 @@ import (
 	"github.com/unrolled/render"
 )
 
+//Render :render data
 var Render *render.Render
 
+// Init :init controller methods
 func Init() {
 	Render = render.New()
 }
 
+//HandlHome :handle "/"
 func HandlHome(w http.ResponseWriter, req *http.Request) {
 	Render.HTML(w, http.StatusOK, "index", nil)
 }
