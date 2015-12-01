0a7fa38e4887abc20c8432b25a394d0c9bd8c566
Qiyu Li <0x9357@gmail.com>
2015-12-01 14:04:47 +0800

    add layout

--- controller/index.go
+++ controller/index.go
@@ -6,12 +6,15 @@ import (
 	"github.com/unrolled/render"
 )
 
-//Render :render data
+//Render :render data var
 var Render *render.Render
 
 // Init :init controller methods
 func Init() {
-	Render = render.New()
+	Render = render.New(render.Options{
+		Directory: "templates",
+		Layout:    "layout",
+	})
 }
 
 //HandlHome :handle "/"
